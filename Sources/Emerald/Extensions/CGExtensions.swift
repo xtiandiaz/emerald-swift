64995c05b82f96f90c0c44ceac3ebb770d1739db
Cristian Díaz <cristian.diaz@ouraring.com>
2021-07-31 09:01:16 +0300

    Add some more CGExtensions

--- Sources/Emerald/Extensions/CGExtensions.swift
+++ Sources/Emerald/Extensions/CGExtensions.swift
@@ -9,12 +9,32 @@ import CoreGraphics
 
 public extension CGPoint {
     
+    static var up: CGPoint {
+        CGPoint(x: 0, y: 1)
+    }
+    
+    static var right: CGPoint {
+        CGPoint(x: 1, y: 0)
+    }
+    
+    static var down: CGPoint {
+        CGPoint(x: 0, y: -1)
+    }
+    
+    static var left: CGPoint {
+        CGPoint(x: -1, y: 0)
+    }
+    
     static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
         CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
     
     static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
         CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    static func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
     }
 }
 
