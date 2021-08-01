f25736e92853b1b7e269132dd27957028d9da790
Cristian Díaz <cristian.diaz@ouraring.com>
2021-08-01 17:55:31 +0300

    Add Dependency Injection utils

---
+++ Sources/Emerald/PropertyWrappers/Inject.swift
@@ -0,0 +1,24 @@
+//
+//  Inject.swift
+//  Sokomatch
+//
+//  Created by Cristian Díaz on 28.2.2021.
+//  Copyright © 2021 Berilio. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct Inject<T> {
+    
+    public var wrappedValue: T {
+        mutating get { value }
+        set { }
+    }
+    
+    public init() {}
+    
+    // MARK: Private
+    
+    private lazy var value = try! InjectionController.shared.resolve(T.self)
+}
