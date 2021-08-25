ba1377b7b8860c3f6fde1387f37788b19d32a663
Cristian Díaz <cristian.diaz@ouraring.com>
2021-08-25 19:21:42 +0300

    Add SKSceneExtensions

---
+++ Sources/Emerald/Extensions/SKSceneExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  SKSceneExtensions.swift
+//  Emerald
+//
+//  Created by Cristian Díaz on 25.8.2021.
+//
+
+import SpriteKit
+
+public extension SKScene {
+    
+    var center: CGPoint {
+        size.center
+    }
+}
