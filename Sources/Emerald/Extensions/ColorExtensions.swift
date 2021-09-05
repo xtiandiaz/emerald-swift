4c9d060cedba166fec0ad8bcc90393f23a7dcd1c
Cristian Díaz <cristian.diaz@ouraring.com>
2021-09-05 23:53:39 +0300

    Add color extensions for lightening and darkening

--- Sources/Emerald/Extensions/ColorExtensions.swift
+++ Sources/Emerald/Extensions/ColorExtensions.swift
@@ -9,6 +9,8 @@ import UIKit
 
 public extension UIColor {
     
+    typealias Components = (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+    
     var accessibleFontColor: UIColor {
         var comps: Components = (red: 0, green: 0, blue: 0, alpha: 1)
         
@@ -31,24 +33,41 @@ public extension UIColor {
             alpha: lhsComps.alpha * rhsComps.alpha)
     }
     
-}
-
-// MARK: - Internal
-
-extension UIColor {
-    
-    typealias Components = (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
-}
-
-// MARK: - Private
-
-private extension UIColor {
-    
-    private func isLightColor(comps: Components) -> Bool {
+    func isLightColor(comps: Components) -> Bool {
         let lightRed = comps.red > 0.65
         let lightGreen = comps.green > 0.65
         let lightBlue = comps.blue > 0.65
         
         return [lightRed, lightGreen, lightBlue].reduce(0) { $1 ? $0 + 1 : $0 } >= 2
+    }
+    
+    func lightened(by factor: CGFloat) -> UIColor {
+        var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        
+        if getHue(&h, saturation: &s, brightness: &b, alpha: &a) {
+            return UIColor(
+                hue: h,
+                saturation: min(max(s - factor * s, 0), 1),
+                brightness: max(min(b + factor * (1 - b), 1), 0),
+                alpha: a
+            )
+        }
+        
+        return self
+    }
+    
+    func darkened(by factor: CGFloat) -> UIColor {
+        var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        
+        if getHue(&h, saturation: &s, brightness: &b, alpha: &a) {
+            return UIColor(
+                hue: h,
+                saturation: s,
+                brightness: max(min(b - abs(factor) * b, 1), 0),
+                alpha: a
+            )
+        }
+        
+        return self
     }
 }
