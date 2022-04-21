9cbe7d7d9ff3b2b576054a1ebefdee93eb348e0e
Cristian Diaz <xtndiaz@gmail.com>
2022-04-21 11:37:13 -0500

    Add OptionalExtensions

---
+++ Sources/Emerald/Extensions/OptionalExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  OptionalExtensions.swift
+//  Emerald
+//
+//  Created by Cristian Diaz on 21.4.2022.
+//
+
+import Foundation
+
+public extension Optional {
+    
+    var isNil: Bool {
+        self == nil
+    }
+}
