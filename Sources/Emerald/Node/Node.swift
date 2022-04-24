0dd087dfeda254ecb56acc360d28733e571e2fc4
Cristian Diaz <xtndiaz@gmail.com>
2022-04-24 17:27:04 -0500

    Set up NodeBehavior base class. Implement Dragging as behavior

--- Sources/Emerald/Node/Node.swift
+++ Sources/Emerald/Node/Node.swift
@@ -5,9 +5,22 @@
 //  Created by Cristian Díaz on 4.9.2021.
 //
 
+import Combine
+import Foundation
 import SpriteKit
 
 open class Node: SKNode {
+    
+    public private(set) lazy var uponTouchesBegan: AnyPublisher<Set<UITouch>, Never> = touchesBeganSubject
+        .eraseToAnyPublisher()
+    
+    public private(set) lazy var uponTouchesMoved: AnyPublisher<Set<UITouch>, Never> = touchesMovedSubject
+//        .share()
+        .eraseToAnyPublisher()
+    
+    public private(set) lazy var uponTouchesEnded: AnyPublisher<Set<UITouch>, Never> = touchesEndedSubject
+//        .share()
+        .eraseToAnyPublisher()
     
     public var width: CGFloat {
         frame.width
@@ -29,4 +42,22 @@ open class Node: SKNode {
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesBeganSubject.send(touches)
+    }
+    
+    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesMovedSubject.send(touches)
+    }
+    
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEndedSubject.send(touches)
+    }
+    
+    // MARK: - Private
+    
+    private let touchesBeganSubject = PassthroughSubject<Set<UITouch>, Never>()
+    private let touchesMovedSubject = PassthroughSubject<Set<UITouch>, Never>()
+    private let touchesEndedSubject = PassthroughSubject<Set<UITouch>, Never>()
 }
