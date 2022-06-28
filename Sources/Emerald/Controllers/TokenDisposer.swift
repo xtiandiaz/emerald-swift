388d5c538b63bfe5706b5a4f632540992c76c1b6
Cristian Diaz <xtndiaz@gmail.com>
2022-06-27 21:21:29 -0500

    Expand TokenDisposer and make public

--- Sources/Emerald/Controllers/TokenDisposer.swift
+++ Sources/Emerald/Controllers/TokenDisposer.swift
@@ -8,15 +8,30 @@
 import Foundation
 import SpriteKit
 
-struct TokenDisposer {
+public struct TokenDisposer {
     
-    func disposeIfInvalidatedOf(token: AnyToken) {
+    public init() {
+    }
+    
+    public func disposeIfInvalidatedOf(tokens: [AnyToken]) {
+        tokens.forEach {
+            disposeIfInvalidatedOf(token: $0)
+        }
+    }
+    
+    public func disposeIfInvalidatedOf(token: AnyToken) {
         if token.isInvalidated {
             disposeOf(token: token)
         }
     }
     
-    func disposeOf(token: AnyToken) {
+    public func disposeOf(tokens: [AnyToken]) {
+        tokens.forEach {
+            disposeOf(token: $0)
+        }
+    }
+    
+    public func disposeOf(token: AnyToken) {
         if let disposalAction = token.disposalAction() {
             token.run(disposalAction, withKey: "disposal") { [unowned token] in
                 token.removeFromParent()
